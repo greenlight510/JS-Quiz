1a7025b6c32e5d202043cbece3661c5205f75a22
greenlight510 <90018321+greenlight510@users.noreply.github.com>
2021-10-18 15:52:28 -0700

    adding javascript

---
+++ assets/js/end.js
@@ -0,0 +1,7 @@
+const player = document.querySelector("#player")
+const sScoreBtn = document.querySelector("#sScoreBtn")
+const fScore = document.querySelector("#fScore")
+const recentScore = document.querySelector("#recentScore")
+
+
+
