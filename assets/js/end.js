df127ec6ce6abb877273b508656c7c6ac4c38c15
greenlight510 <90018321+greenlight510@users.noreply.github.com>
2021-10-18 20:56:44 -0700

    topplayers update

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,7 +1,38 @@
 const player = document.querySelector("#player")
 const sScoreBtn = document.querySelector("#sScoreBtn")
 const fScore = document.querySelector("#fScore")
-const recentScore = document.querySelector("#recentScore")
+const recentScore = localStorage.getItem("recentScore")
 
 
+const hScores = JSON.parse(localStorage.getItem('hScores')) || []
 
+const M_H_SCORES = 4
+
+fScore.innerText = recentScore
+
+player.addEventListener('keyup', () => {
+    sScoreBtn.disabled = !player.value
+})
+
+sHighScore = e => {
+    e.preventDefault()
+
+    const score =  {
+        score: recentScore,
+        name: player.value
+    }
+
+    hScores.push(score)
+
+    hScores.sort((a,b) => {
+
+        return b.score - a.score
+    })
+
+    hScores.splice(4)
+
+    localStorage.serItem('hScores', JSON.stringify(hScores))
+
+    window.location.assign('/')
+
+}
---
+++ assets/js/topplayers.js
@@ -0,0 +1,7 @@
+const hScoresList = document.querySelector("#hScoresList")
+const hScores = JSON.parse(localStorage.getItem("hScores")) || []
+
+hScoresList.innerHTML =
+hScores.map(score => {
+    return `<li class = "high-score">${score.name} - ${score.score}</li>`
+}).join('')
